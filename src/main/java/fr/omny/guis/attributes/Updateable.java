bbb4d5f92503eff9e8c700404cf2fed3ff264c4b
FabienCAYRE <fabiencayre81@gmail.com>
2022-12-30 14:42:56 +0100

    Provide which field was edited

--- src/main/java/fr/omny/guis/attributes/Updateable.java
+++ src/main/java/fr/omny/guis/attributes/Updateable.java
@@ -1,5 +1,7 @@
 package fr.omny.guis.attributes;
 
+import java.lang.reflect.Field;
+
 /**
  * Behaviour of something that need to listen to field and global updates
  *
@@ -11,7 +13,7 @@ public interface Updateable {
   /**
    * Method called when a single field was edited by the player
    */
-  default void fieldUpdate() {}
+  default void fieldUpdate(Field field) {}
 
   /**
    * Method called when the inventory has been closed
