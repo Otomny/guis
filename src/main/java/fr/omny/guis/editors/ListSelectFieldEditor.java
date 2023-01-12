302b6bc996424bb3f23febf7e05d437f285b96c0
Fabien CAYRE <fabiencayre81@gmail.com>
2023-01-12 09:27:48 +0100

    Select object in list editor

--- src/main/java/fr/omny/guis/editors/ListSelectFieldEditor.java
+++ src/main/java/fr/omny/guis/editors/ListSelectFieldEditor.java
@@ -1,9 +1,8 @@
 package fr.omny.guis.editors;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -12,43 +11,14 @@ import fr.omny.guis.OField;
 import fr.omny.guis.OFieldEditor;
 import fr.omny.guis.backend.GuiItemBuilder;
 import fr.omny.guis.backend.GuiListBuilder;
-import fr.omny.guis.editors.providers.EnchantmentListProvider;
-import fr.omny.guis.editors.providers.ObjectInListProvider;
 import fr.omny.guis.utils.ReflectionUtils;
 import fr.omny.guis.utils.Utils;
 
-/**
- * You can only set this by using the editor properties of {@link OField}
- * annotation
- */
-public class ObjectInListFieldEditor implements OFieldEditor {
-
-  private static List<ObjectInListProvider<?>> list = new ArrayList<>();
-
-  /**
-   * @param provider
-   */
-  public static void register(ObjectInListProvider<?> provider) {
-    list.add(provider);
-  }
-
-  @SuppressWarnings("unchecked")
-  public static Optional<ObjectInListProvider<Object>>
-  findProvider(Class<?> type) {
-    return list.stream()
-        .filter(p -> p.type() == type)
-        .map(p -> (ObjectInListProvider<Object>)p)
-        .findFirst();
-  }
-
-  static {
-    // Initializer
-    register(new EnchantmentListProvider());
-  }
+public class ListSelectFieldEditor implements OFieldEditor {
 
   @Override
   public boolean allowNullValues() {
-    return true;
+    return false;
   }
 
   @Override
@@ -68,21 +38,22 @@ public class ObjectInListFieldEditor implements OFieldEditor {
   private void edit(int page, Player player, Object toEdit, Field field,
                     OField fieldData, Runnable onClose) {
     Class<?> type = field.getType();
-    var optionalProvider = findProvider(type);
+    var optionalProvider = ObjectInListFieldEditor.findProvider(type);
     if (optionalProvider.isEmpty()) {
       player.sendMessage("§cNo provider found for type " +
                          type.getSimpleName());
       onClose.run();
       return;
     }
-		var provider = optionalProvider.get();
-    Object selectedValue = ReflectionUtils.get(toEdit, field);
+    var provider = optionalProvider.get();
+    Collection<Object> listValue =
+        (Collection<Object>)ReflectionUtils.get(toEdit, field);
     new GuiListBuilder<>(Utils.replaceColor(Utils.orString(
                              fieldData.value(), "&e" + field.getName())),
                          provider.provide())
         .page(page)
         .itemCreation(obj -> {
-          boolean selected = selectedValue == obj;
+          boolean selected = listValue.contains(obj);
           return new GuiItemBuilder()
               .name((selected ? "§b" : "§e") + provider.asString(obj))
               .icon(selected ? Material.DIAMOND : fieldData.display())
@@ -90,7 +61,11 @@ public class ObjectInListFieldEditor implements OFieldEditor {
               .description("§7§oValue: §e" + provider.asString(obj))
               .click((p, slot, click) -> {
                 try {
-                  ReflectionUtils.set(toEdit, field, obj);
+                  if (selected) {
+                    listValue.remove(obj);
+                  } else {
+                    listValue.add(obj);
+                  }
                 } catch (Exception e) {
                   e.printStackTrace();
                 }
