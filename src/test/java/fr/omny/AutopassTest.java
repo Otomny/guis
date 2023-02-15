56d683a8314041365e74bc3aa8b9998b50962c48
FabienCAYRE <fabiencayre81@gmail.com>
2023-02-15 12:55:55 +0100

    Autopass tests

---
+++ src/test/java/fr/omny/AutopassTest.java
@@ -0,0 +1,18 @@
+package fr.omny;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Autopass test for GitHub actions
+ */
+public class AutopassTest {
+
+	@Test
+	public void yepp() {
+		assertTrue(true);
+	}
+
+}
