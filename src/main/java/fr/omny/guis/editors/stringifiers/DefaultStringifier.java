e55e444dbfbd7971efeba6feb02033510827cd98
FabienCAYRE <fabiencayre81@gmail.com>
2023-02-28 18:44:53 +0100

    Way to implement new default stringifier

--- src/main/java/fr/omny/guis/editors/stringifiers/DefaultStringifier.java
+++ src/main/java/fr/omny/guis/editors/stringifiers/DefaultStringifier.java
@@ -1,12 +1,16 @@
 package fr.omny.guis.editors.stringifiers;
 
+import java.util.function.Function;
+
 import fr.omny.guis.utils.ReflectionUtils;
 
-public class DefaultStringifier implements Stringifier{
+public class DefaultStringifier implements Stringifier {
+
+	public static Function<Object, String> STRINGIFIER = ReflectionUtils::string;
 
 	@Override
 	public String toString(Object object) {
-		return ReflectionUtils.string(object);
+		return STRINGIFIER.apply(object);
 	}
-	
+
 }
