e2a0c2a5ae34b98b64e9defc4ffb05921f89a32c
FabienCAYRE <fabiencayre81@gmail.com>
2023-04-18 14:55:51 +0200

    Possible additional data for item field

--- src/main/java/fr/omny/guis/fields/ItemField.java
+++ src/main/java/fr/omny/guis/fields/ItemField.java
@@ -1,8 +1,9 @@
 package fr.omny.guis.fields;
 
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.bukkit.Material;
@@ -33,12 +34,42 @@ public class ItemField implements Itemable, Cloneable {
 	@OField(display = Material.ENCHANTED_BOOK)
 	private List<EnchantmentField> enchantments = new ArrayList<>();
 
-	public ItemField() {}
+	private Map<String, Object> additionalData = new HashMap<>();
 
+	/**
+	 * Create an empty item field
+	 */
+	public ItemField() {
+	}
+
+	/**
+	 * Create an item field with the given material, quantity and enchantments
+	 * 
+	 * @param type         The material
+	 * @param quantity     The quantity
+	 * @param enchantments The enchantments
+	 */
 	public ItemField(Material type, int quantity, List<EnchantmentField> enchantments) {
 		this.type = type;
 		this.quantity = quantity;
 		this.enchantments = enchantments;
+	}
+
+	/**
+	 * Create an item field with the given material, quantity, enchantments and
+	 * additional data
+	 * 
+	 * @param type           The material
+	 * @param quantity       The quantity
+	 * @param enchantments   The enchantments
+	 * @param additionalData The additional data
+	 */
+	public ItemField(Material type, int quantity, List<EnchantmentField> enchantments,
+			Map<String, Object> additionalData) {
+		this.type = type;
+		this.quantity = quantity;
+		this.enchantments = enchantments;
+		this.additionalData = additionalData;
 	}
 
 	@Override
@@ -52,15 +83,15 @@ public class ItemField implements Itemable, Cloneable {
 	 * @return The itemstack
 	 */
 	public ItemStack asItem() {
-		var item = new ItemStack(type, quantity); 
+		var item = new ItemStack(type, quantity);
 		this.enchantments.forEach(e -> e.applyEnchantment(item));
 		return item;
 	}
 
 	@Override
 	public ItemField clone() {
-		return new ItemField(this.type, this.quantity, 
-			this.enchantments.stream().map(EnchantmentField::clone).collect(Collectors.toList()));
+		return new ItemField(this.type, this.quantity,
+				this.enchantments.stream().map(EnchantmentField::clone).collect(Collectors.toList()));
 	}
 
 	@Override
